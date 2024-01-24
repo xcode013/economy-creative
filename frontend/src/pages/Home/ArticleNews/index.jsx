7a9aeddc0ff39305d742ede204a2ccc0ea8823c5
PaixCode <rythoryzer@gmail.com>
2024-01-24 10:16:51 +0700

    update on Home page - ArticleNews section

--- frontend/src/pages/Home/ArticleNews/index.jsx
+++ frontend/src/pages/Home/ArticleNews/index.jsx
@@ -9,13 +9,13 @@ function ArticleNews() {
     <section className="articles">
       <header>
         <FadeIn>
-          <h2 className="subheadline heading-sm">Creative Updates</h2>
+          <h2 className="subheadline heading-sm">Creative Update News</h2>
         </FadeIn>
         <FadeIn delay={1}>
           <h1 className="headline heading-md">Articles & News</h1>
         </FadeIn>
       </header>
-        <NewsListSlider newsData={newsDatas} />
+      <NewsListSlider newsData={newsDatas} />
     </section>
   );
 }
